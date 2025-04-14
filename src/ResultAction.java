427d1dcbd99ef4c88765c311fe86067c0049fe94
'koba' <'hrs2421446@stu.o-hara.ac.jp'>
2025-04-15 08:33:48 +0900

    基本的な動作のためのActionクラスの追加

---
+++ src/ResultAction.java
@@ -0,0 +1,13 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class ResultAction extends Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+}

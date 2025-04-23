b502a6ae313dc57fc332cfeda644deb56b7bce5a
'koba' <'hrs2421446@stu.o-hara.ac.jp'>
2025-04-23 12:45:36 +0900

    問題画面のJavaScript改良

--- WebContent/js/select.js
+++ WebContent/js/select.js
@@ -1,96 +1,107 @@
-var choiceElm = document.querySelectorAll(".choice");
-var textElm = document.querySelector(".text");
-var popupElm = document.querySelector(".popup");
-var canBtnElm = document.querySelector(".cancel-button");
-var okBtnElm = document.querySelector(".ok-button");
-// ポップアップが表示されていればtrueにする
-var popupFlag = false;
-var select = null;
+class ChoiceMove {
+    init() {
+        this.choiceElm = document.querySelectorAll(".choice");
+        this.textElm = document.querySelector(".text");
+        this.popupElm = document.querySelector(".popup");
+        const canBtnElm = document.querySelector(".cancel-button");
+        const okBtnElm = document.querySelector(".ok-button");
+        // ポップアップが表示されていればtrueにする
+        this.popupFlag = false;
+        let select = null;
 
-// フォームを送る関数
-function postForm(value) {
-    // formの作成
-    var form = document.createElement("form");
-    var request = document.createElement("input");
-
-    form.method = 'POST';
-    form.action = 'StandBy.action';
-
-    request.type = 'hidden';
-    request.name = 'choice';
-    request.value = value;
-
-    form.appendChild(request);
-    document.body.appendChild(form);
-
-    form.submit();
-}
-
-// ポップアップを消す関数
-function popout() {
-    if (popupFlag) {
-        popupElm.style.zIndex = -1;
-        canGrayout();
-        popupFlag = false;
-    }
-}
-
-// 全体を具レイアウトするための関数
-function grayout() {
-    for (const elm of choiceElm) {
-        elm.classList.add('grayout');
-    }
-    textElm.classList.add('grayout');
-};
-
-// グレイアウトを解除するための関数
-function canGrayout() {
-    for (const elm of choiceElm) {
-        elm.classList.remove('grayout');
-    }
-    textElm.classList.remove('grayout');
-};
-
-// 左右の選択肢でのイベント
-for (const elm of choiceElm) {
-    // マウスがホバーされてきたらハイライトする
-    elm.addEventListener('mouseover', function() {
-        if (!popupFlag) {
-            elm.classList.add("highlight");
-        };
-    });
-    // マウスが離れたらハイライトを解除
-    elm.addEventListener('mouseleave', function() {
-        if (!popupFlag) {
-            elm.classList.remove("highlight");
+        // 左右の選択肢でのイベント
+        for (const elm of this.choiceElm) {
+            // マウスがホバーされてきたらハイライトする
+            elm.addEventListener('mouseover', () => {
+                if (!this.popupFlag) {
+                    elm.classList.add("highlight");
+                };
+            });
+            // マウスが離れたらハイライトを解除
+            elm.addEventListener('mouseleave', () => {
+                if (!this.popupFlag) {
+                    elm.classList.remove("highlight");
+                }
+            });
+            // クリックしたら全体がグレーアウトしてポップアップが出てくる
+            elm.addEventListener('click', () => {
+                if (elm.classList.contains('left')) {
+                    select = 'A';
+                } else {
+                    select = 'B';
+                }
+                this.popupFlag = true;
+                this.popup(elm);
+            });
         }
-    });
-    // クリックしたら全体がグレーアウトしてポップアップが出てくる
-    elm.addEventListener('click', function() {
-        if (elm.classList.contains('left')) {
-            select = 'A';
-        } else {
-            select = 'B';
-        }
-        popupFlag = true;
-        grayout();
+
+        // キャンセルボタンが押されたらポップアップを解除する
+        canBtnElm.addEventListener('click', () => {
+            this.popout();
+        })
+
+        // okボタンが押されたらフォームを送信する
+        okBtnElm.addEventListener('click', () => {
+            // 選択せずにokを押したらポップアップが消えるだけ
+            if (this.popupFlag & select != null) {
+                this.postForm(select);
+            } else {
+                this.popout();
+            }
+        })
+
+    };
+
+    // フォームを送る関数
+    postForm(value) {
+        // formの作成
+        const form = document.createElement("form");
+        const request = document.createElement("input");
+
+        form.method = 'POST';
+        form.action = 'StandBy.action';
+
+        request.type = 'hidden';
+        request.name = 'choice';
+        request.value = value;
+
+        form.appendChild(request);
+        document.body.appendChild(form);
+
+        form.submit();
+    };
+
+    popup(elm) {
         elm.classList.remove("highlight");
-        popupElm.style.zIndex = 10;
-    });
+        this.grayout();
+        this.popupElm.style.zIndex = 10;
+    }
+
+    // ポップアップを消す関数
+    popout() {
+        if (this.popupFlag) {
+            this.popupElm.style.zIndex = -1;
+            this.canGrayout();
+        }
+    }
+
+    // 全体を具レイアウトするための関数
+    grayout() {
+        for (const elm of this.choiceElm) {
+            elm.classList.add('grayout');
+        }
+        this.textElm.classList.add('grayout');
+    };
+
+    // グレイアウトを解除するための関数
+    canGrayout() {
+        for (const elm of this.choiceElm) {
+            elm.classList.remove('grayout');
+        }
+        this.textElm.classList.remove('grayout');
+    };
+
 }
 
-// キャンセルボタンが押されたらポップアップを解除する
-canBtnElm.addEventListener('click', function() {
- popout();
-})
-
-// okボタンが押されたらフォームを送信する
-okBtnElm.addEventListener('click', function() {
-    // 選択せずにokを押したらポップアップが消えるだけ
-    if (popupFlag & select != null) {
-        console.log(select);
-        postForm(select);
-    } else {
-        popout();
-    }
-})
+const choiceMove = new ChoiceMove();
+choiceMove.init();
